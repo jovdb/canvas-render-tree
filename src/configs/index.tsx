adc2a525eeb2c96360cc08b2fb52673cffd72cdf
Jo Van den Berghe <jo.vandenberghe@smartphoto.com>
2026-06-23 23:20:15 +0200

    Make it posisble to default config as JSON

---
+++ src/configs/index.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import { ItemConfigFn } from "../canvas";
+import { RenderItemName } from "../operations";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const configs: Partial<Record<RenderItemName, ItemConfigFn<any>>> = {} as const;
+
+export function addRendererConfig(
+  name: RenderItemName,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  rendererConfig: ItemConfigFn<any>,
+) {
+  // Disabled for hot module reloading
+  // if (configs[name as RenderItemName]) {
+  //   throw new Error(`Renderer config for '${name}' already exists`);
+  // }
+  configs[name as RenderItemName] = rendererConfig;
+}
+
+export function getRendererConfig(
+  name: RenderItemName | undefined,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+): ItemConfigFn<any> | undefined {
+  if (!name) return undefined;
+  const configComponent = configs[name as RenderItemName];
+  return configComponent ?? JsonConfig;
+}
+
+export const JsonConfig: ItemConfigFn<object> = ({ config, mutateConfig }) => {
+  const [value, setValue] = useState(JSON.stringify(config, null, 2));
+  const [error, setError] = useState("");
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        // Get value of textarea
+        const value = (e.target as HTMLFormElement).elements.namedItem(
+          "json",
+        ) as HTMLTextAreaElement;
+        try {
+          const parsed = JSON.parse(value.value);
+          mutateConfig((config) => Object.assign(config, parsed));
+          setError("");
+        } catch (err) {
+          console.error("Invalid JSON", err);
+          setError("Invalid JSON: " + (err as Error).message);
+        }
+      }}
+    >
+      <strong>JSON:</strong>{" "}
+      <button type="submit" style={{ marginBottom: "10px" }}>
+        Update
+      </button>
+      <br />
+      <span style={{ color: "red" }}>{error}</span>
+      <textarea
+        name="json"
+        style={{ fontFamily: "monospace", width: "100%", height: "200px" }}
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
+    </form>
+  );
+};
